b2feb45d6129d749088c70b3e9290af7ca7c7d33
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-21 13:26:57 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='agaro',
-    version='0.1.1',
+    version='0.2.0',
     description="Framework to run models",
     long_description=readme + '\n\n' + history,
     author="Elliot Marsden",
